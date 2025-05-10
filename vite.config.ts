111597c1870277d5841ecbccd8ce4ae8f173fde1
million-art <millionmulugeta09@gmail.com>
2025-05-10 19:02:12 +0300

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
+
     react(),
     tsconfigPaths(),
   ],
